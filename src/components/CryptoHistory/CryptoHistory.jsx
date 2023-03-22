148277a431292fa00ebac6387ddc98c4709d3db2
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-22 12:57:20 +0200

    task 3 done

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,30 +1,26 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 import { transactionDate } from 'utils/dataCreator';
 
-export const CryptoHistory = ({ items }) => {
+export const CryptoHistory = ({items}) => {
   return (
-    <>
-      <BaseTable>
-        <THead>
-          <tr>
-            <Th>№</Th>
-            <Th>PRICE</Th>
-            <Th>AMOUNT</Th>
-            <Th>DATE</Th>
-          </tr>
-        </THead>
-
-        <tbody>
-          {items.map(({ id, price, amount, date }, idx) => (
-            <Tr key={id}>
-              <Td>{idx + 1}</Td>
-              <Td>{price}</Td>
-              <Td>{amount}</Td>
-              <Td>{transactionDate(date)}</Td>
-            </Tr>
-          ))}
-        </tbody>
-      </BaseTable>
-    </>
-  );
-};
+  <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+<tbody>
+  {items.map(({ id, price, amount, date }, idx) => (
+    <Tr key={id}>
+      <Td>{idx + 1}</Td>
+      <Td>{price}</Td>
+      <Td>{amount}</Td>
+      <Td>{transactionDate(date)}</Td>
+    </Tr>
+  ))}
+</tbody>
+</BaseTable>);
+}; 
